49ef087ce86669465243486b63b3e07e835371f3
= <=>
2024-11-10 05:31:35 -0500

    change s3 bucket region to east

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-proj-1-remote-state-bucket-1234567"
+    bucket = "my-terraform-proj-remote-state-bucket-1234567"
     key    = "devops-project-1/terraform.tfstate"
-    region = "us-west-1"
+    region = "us-east-1"
   }
 }
