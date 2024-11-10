e5173da81de3cd473589965886e61b7023079f6f
= <=>
2024-11-09 23:41:09 -0500

    change the resource name of S3

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-dev-proj-1-remote-state-bucket-123456"
+    bucket = "my-terraform-proj-1-remote-state-bucket-1234567"
     key    = "devops-project-1/terraform.tfstate"
     region = "us-west-1"
   }
